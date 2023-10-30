6165a564f6dc66452a8ce0c7ce22a99d7c6dfcc8
Osha Groetz <ogroetz@gmail.com>
2023-10-30 20:41:22 +0000

    end

--- app.rb
+++ app.rb
@@ -2,13 +2,18 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  # "
+  # <h1>Welcome to your Sinatra App!</h1>
+  # <p>Define some routes in app.rb</p>
+  # "
+  erb(:homepage)
 end
 
 get("/process_results") do
-  @num_dice = params.fetch("").to_i
-  @num_sides = params.fetch("").to_i
+  @num_dice = params.fetch("dice_count").to_i
+  @num_sides = params.fetch("sides").to_i
+
+  
+
+  erb(:results)
 end
